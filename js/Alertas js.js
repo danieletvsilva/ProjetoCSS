d6b6aadbf51a76d5ed94784ba7b34690c045f8ae
Daniele Torres <danieletvsilva@gmail.com>
2023-04-16 15:47:45 -0300

    Feature: Ajustes e inclusão de JS

--- js/Alertas js.js
+++ js/Alertas js.js
@@ -1,3 +1,11 @@
 function alertaMensagem(){
-    alert("Sua mensagem foi enviada")
+    alert("Sua mensagem foi enviada");
 }
+
+function abrirPopUp() {
+    window.open('Popup.html', 'pagina', "width=1200, height=550, top=200, left=300");
+  }  
+
+function fecharPopUp() {
+    window.close();
+  }
